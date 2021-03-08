6c038f485e3b028e0ddc486344878729e5fb2c5f
Robert Schrenk <robert@schrenk.cc>
2021-03-08 20:41:14 +0100

    Enhance list view.

--- pages/list.php
+++ pages/list.php
@@ -118,6 +118,13 @@ if (empty($channel)) {
         $package->maintainer_default = $maintainer_default;
         $package->maintainer_etapas = $maintainer_etapas;
         $package->maintainer_eduthek = $maintainer_eduthek;
+        $package->exclamation = (
+            !empty($package->default_publishas) && empty($package->default_active)
+            ||
+            !empty($package->etapas_publishas) && empty($package->etapas_active)
+            ||
+            !empty($package->eduthek_publishas) && empty($package->eduthek_active)
+        );
         echo $OUTPUT->render_from_template(
             'block_edupublisher/maintain_table_row',
             $package
