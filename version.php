531f04e69ba978ed0617659036b45866f0b51257
prowebat <1819129+prowebat@users.noreply.github.com>
2025-01-10 20:06:31 +0800

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025010500;
+$plugin->version = 2025011000;
 $plugin->requires = 2014051200;
 $plugin->component = 'block_edupublisher';
 $plugin->release = '2.2';
